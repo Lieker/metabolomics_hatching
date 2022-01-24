11e675cd21e7635c47d6e7a6a97d3fb88814eb66
lieker <l.e.vlaar@uva.nl>
2022-01-24 16:19:13 +0100

    improved fractions figures

--- scripts/ordermatrixforcorplot_fr.R
+++ scripts/ordermatrixforcorplot_fr.R
@@ -6,7 +6,7 @@ ordermatrixforcorplot_fr <- function(x, compounds, hatching = hatch_s_subset$mea
   c <- c[order(-c$hatch),]
   
   #get only top and bottom 10
-  c1 <- c[c(1:11, (length(c) - 9):(length(c))),]
+  c1 <- c[c(1:11),]
   rn <- row.names(c1)
   c1 <- c1[, which(names(c1) %in% rn)] %>% as.matrix()
   
@@ -31,4 +31,47 @@ ordermatrixforcorplot_fr <- function(x, compounds, hatching = hatch_s_subset$mea
   row.names(c1) <- c3$Name
   
   return(c1)
+}
+
+ordermatrixforcorplot_fr2 <- function(x, hatching = hatch_s_subset$mean_hatch){
+  x$hatch <- hatching
+  return(x)
+}
+
+get_testRes_fr <- function(x, compounds, hatching = hatch_s_subset$mean_hatch, t){
+  x$hatch <- hatching
+  c <- as.data.frame(cor(x))
+  c <- c[order(-c$hatch),]
+  
+  #get only top and bottom 10
+  c1 <- c[c(1:11),]
+  rn <- row.names(c1)
+  c1 <- c1[, which(names(c1) %in% rn)] %>% as.matrix()
+  
+  #order the matrices
+  c1 <- as.data.frame(c1[rn, rn])
+  
+  #order also the testRes matrix
+  testRes <- as.data.frame(t)
+  testRes <- testRes[which(names(testRes) %in% names(c1)), which(names(testRes) %in% names(c1))]
+  testRes <- testRes[rn, rn]
+  
+  #change names to compoundnames
+  rn <- as.data.frame(rn)
+  names(rn) <- "rn"
+  rownames(compounds) <- NULL
+  compounds <- compounds %>% rownames_to_column("rn")
+  c3 <- left_join(rn, compounds[,c("rn","Name","m.z.meas.", "RT..min.")], by = "rn")
+  c3$mz <- substr(c3$m.z.meas., 0, 6)
+  c3$Rt <- substr(c3$RT..min., 0, 5)
+  
+  while(length(ind <- which(c3$Name == "")) > 0){
+    c3$Name[ind] <- paste0("m/z ",c3$m.z.meas.[ind]," Rt ",c3$RT..min.[ind])
+  }
+  c3$Name[c3$rn == "hatch"] <- "hatch"
+  row.names(testRes) <- c3$Name
+  testRes <- t(testRes)
+  row.names(testRes) <- c3$Name
+  
+  return(testRes)
 }
